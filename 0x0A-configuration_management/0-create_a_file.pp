1c1ac5def4329e64666011544c737a8e60154a87
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-08-06 13:40:15 +0000

    [fix] correct mode file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-# create a file in /tmp.
+# create a file in tmp
 file { 'holberton':
   path    => '/tmp/holberton',
-  mode    => '0644',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet';
