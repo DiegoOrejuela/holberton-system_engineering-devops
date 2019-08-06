4adaa7be7457806c66b4d3ef8b6d5871e4e2ba89
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-08-06 23:00:03 +0000

    T4: solved puppet-lint

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,4 +1,5 @@
+# Let’s practice using Puppet to make changes to our configuration file
 exec { 'change_configuration_file':
-     command => '/bin/echo -e "PasswordAuthentication no\nIdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
-     path    => '/usr/bin';
+  command => '/bin/echo -e "PasswordAuthentication no\nIdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+  path    => '/usr/bin';
 }
